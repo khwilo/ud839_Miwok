c1342c3bcbb4b9c743a0a3bef80abf7b5937f801
Khwilo Kabaka <kwhyzzy@gmail.com>
2018-05-25 21:22:13 +0300

    Modify the adapter to include an image resource

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -41,6 +42,11 @@ public class WordAdapter extends ArrayAdapter<Word> {
         // populate them onto the TextViews.
         miwokTextView.setText(word.getMiwokTranslation());
         defaultTextView.setText(word.getDefaultTranslation());
+
+        // Find the ImageView in the list_item.xml layout file and
+        // populate it with an image asset.
+        ImageView imageView = (ImageView) listItemView.findViewById(R.id.word_image_view);
+        imageView.setImageResource(word.getImageResource());
 
         // Return the list item layout to be shown on the ListView.
         return listItemView;
